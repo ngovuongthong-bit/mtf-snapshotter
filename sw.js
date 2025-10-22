c1041c082404acb2129a28e435e386ca1e3f732c
ngovuongthong-bit <ngovuongthong@gmail.com>
2025-10-23 01:51:56 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('omega-lite-plus-v1').then(c=>c.addAll([
-    './omega-lite-plus.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
